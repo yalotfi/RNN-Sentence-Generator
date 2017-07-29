9aac1d70158ccd1a27c8ed55c003ea4ef52eee78
Yaseen Lotfi <yalotfi@outlook.com>
2017-07-29 15:58:43 -0400

    passing ending generator on test stories - need attention mechanism

--- generator.py
+++ generator.py
@@ -1,30 +1,8 @@
 import csv
-import pprint as pp
 import numpy as np
 
 from keras.preprocessing.text import Tokenizer
 from keras.models import load_model
-from keras.models import Sequential
-from keras.layers import Dense, TimeDistributed
-from keras.layers.embeddings import Embedding
-from keras.layers.recurrent import GRU
-
-
-def build_rnn(vocab_size, embeddings, hidden, batch_size, timesteps):
-    model = Sequential()
-    model.add(Embedding(batch_input_shape=(batch_size, timesteps),
-                        input_dim=vocab_size + 1,
-                        output_dim=embeddings,
-                        mask_zero=True))
-    model.add(GRU(hidden,
-                  return_sequences=True,
-                  stateful=True))
-    model.add(GRU(hidden,
-                  return_sequences=True,
-                  stateful=True))
-    model.add(TimeDistributed(Dense(vocab_size + 1, activation='softmax')))
-    model.compile(loss='sparse_categorical_crossentropy', optimizer='adam')
-    return model
 
 
 def load_corpus(read):
@@ -34,9 +12,14 @@ def load_corpus(read):
     return [' '.join(story) for story in story_lists]
 
 
-def main(vocab_path, story_path, model_path):
+def main(model_path, vocab_path, test_path, gen_iters):
+    # Load trained RNN model
+    print("\nLoading Model...\n")
+    rnn_lm = load_model(model_path)  # First main() param
+    print("\nModel Ready...\n")
+
     # Load and process ROC corpus
-    corpus = load_corpus(vocab_path)  # First main() param
+    corpus = load_corpus(vocab_path)  # Second main() param
 
     # Use Keras primitive to tokenize ROC corpus
     token = Tokenizer(lower=True, filters='')
@@ -48,101 +31,63 @@ def main(vocab_path, story_path, model_path):
     end_of_sentence = ['.', '?', '!']  # eos tokens
 
     # Load 25 stories to test generative story endings
-    with open(story_path, 'r') as csvfile:  # Second main() param
+    with open(test_path, 'r') as csvfile:  # Third main() param
         reader = csv.reader(csvfile)
         test_stories = []
-        for story in reader:
-            # Test file has a blank column, just remove it if it exists
-            if len(story) == 6:
-                del story[-1]
-                test_stories.append(story)
-            else:
-                test_stories.append(story)
+        row = 0
+        while row < gen_iters:
+            for story in reader:
+                # Test file has a blank column, just remove it if it exists
+                if len(story) == 6:
+                    del story[-1]
+                    test_stories.append(story)
+                else:
+                    test_stories.append(story)
+            row += 1
     print("Will generate {} endings.\n".format(len(test_stories)))
 
+    # Preprocess the test stories into their contexts, endings, and sequences
     contexts, endings = [], []
     for story in test_stories:
         contexts.append(' '.join(story[:-1]))
         endings.append(story[-1])
     sequences = token.texts_to_sequences(contexts)
-    print("CONTEXT:\n{}\n".format(contexts[0]))
-    print("INPUT SEQUENCE:\n{}\n".format(sequences[0]))
-    print("ENDING:\n{}\n".format(endings[0]))
 
-    ''' TESTING '''
-    # Load trained RNN model
-    print("\nLoading Model...\n")
-    rnn_lm = load_model(model_path)  # Third main() param
-    print("\nModel Ready...\n")
+    # Generate endings for the first i stories loaded into session
+    for i in range(gen_iters):
+        # Format log output
+        print("Story Number: {}".format(i + 1))
+        print("CONTEXT: {}".format(contexts[i]))
+        print("GIVEN ENDING: {}".format(endings[i]))
 
-    input_seq = np.array(sequences[0])[None]  # (1, n) input vector
+        # Instantiate empty data structs for sentence generation
+        input_seq = np.array(sequences[i])[None]  # (1, n) input vector
+        gen_end = []  # Stores each word of the generated endings
+        word_prob = []  # Store probability distributions
+        next_p = None  # Choose from random sample in probability space
 
-    # print(input_seq)
-    # print(input_seq[:, 2])
-    # print(input_seq[:, 2].shape)
+        # Generate probability score for each word in our vocabulary
+        for step in range(input_seq.shape[-1]):
+            word_prob = rnn_lm.predict_on_batch(input_seq[:, step])[0, -1]
 
-    # word_prob = rnn_lm.predict_on_batch(input_seq[:, 0])
-    # print(word_prob)
+        # Sampling from word_prob, find probability of the next word
+        while not gen_end or vocab_lookup[next_p][-1] not in end_of_sentence:
+            next_p = np.random.choice(a=word_prob.shape[-1], p=word_prob)
+            gen_end.append(next_p)
+            word_prob = rnn_lm.predict_on_batch(
+                np.array(next_p)[None, None])[0, -1]
 
-    generated_ending = []
-    word_prob = []  # Store probability distributions
-    for step in range(input_seq.shape[-1]):
-        word_prob = rnn_lm.predict_on_batch(input_seq[:, step])[0, -1]
-    while not generated_ending or vocab_lookup[next_word][-1] not in end_of_sentence:
-        next_word = np.random.choice(a=word_prob.shape[-1], p=word_prob)
-        generated_ending.append(next_word)
-        word_prob = rnn_lm.predict_on_batch(
-            np.array(next_word)[None, None])[0, -1]
-    prob_dist = generated_ending
-    generated_ending = ' '.join([vocab_lookup[word]
-                                 for word in generated_ending])
-    print("Highest Probabilities: {}\n".format(prob_dist))
-    print("GENERATED ENDING: {}\n".format(generated_ending))
-    ''' TESTING '''
+        # Reset the hidden states to not influence the next generation
+        rnn_lm.reset_states()
 
-    # # Generate each word of the fifth sentence until it hits an eos tag
-    # for context, idxs, ending in zip(contexts, context_idxs, endings):
-    #     print("STORY: ", context)
-    #     print("GIVEN ENDING: ", ending)
-    #     generated_ending = []
-    #     idxs = np.array(idxs)[None]
-    #     for step_idx in range(idxs.shape[-1]):
-    #         word_prob = rnn_lm.predict_on_batch(step_idx[:, step_idx])[0, -1]
-    #     while not generated_ending or vocab_lookup[next_word][-1] not in end_of_sentence:
-    #         next_word = np.random.choice(a=word_prob.shape[-1], p=word_prob)
-    #         generated_ending.append(next_word)
-    #         word_prob = rnn_lm.predict_on_batch(
-    #             np.array(next_word)[None, None])[0, -1]
-    #     rnn_lm.reset_states()
-    #     generated_ending = ' '.join([vocab_lookup[word]
-    #                                  for word in generated_ending])
-    #     print("GENERATED ENDING: {}\n".format(generated_ending))
-
-    # # Compile RNN architecture
-    # vocab_size = len(token.word_index)
-    # embeddings = 300
-    # hidden_cells = 500
-    # batch_size = 1
-    # steps = 1
-    # print("\nCompiling model with the following hyperparams:")
-    # print(
-    #     "Vocab: {}\nEmbeddings: {}\nHidden: {}\nBatches: {}\nSteps: {}".format(
-    #         vocab_size, embeddings, hidden_cells, batch_size, steps))
-    # rnn_model = build_rnn(vocab_size=vocab_size,
-    #                       embeddings=embeddings,
-    #                       hidden=hidden_cells,
-    #                       batch_size=batch_size,
-    #                       timesteps=steps)
-    # print("\nModel Compiled...\n")
-
-    # # Load pre-trained weights
-    # print("Loading Trained Weights...\n")
-    # rnn_model.load_weights(weights_path)  # Third main() param
-    # print("Weights Trained Loaded...\n")
+        # Finally go sequence to word from our vocab dictionary and print
+        gen_end = ' '.join([vocab_lookup[word] for word in gen_end])
+        print("GENERATED ENDING: {}\n".format(gen_end))
 
 
 if __name__ == '__main__':
     vocab_path = 'data/roc_stories_full.csv'
     story_path = 'data/test_generation.csv'
     model_path = 'model/rnn_model.h5'
-    main(vocab_path, story_path, model_path)
+    gen_iters = 5
+    main(model_path, vocab_path, story_path, gen_iters)
