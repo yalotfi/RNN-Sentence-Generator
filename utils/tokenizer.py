06fd64c293a9a7b5597f0c9c2b07f52361ba7f23
Yaseen Lotfi <yalotfi@outlook.com>
2017-08-10 00:58:02 -0400

    preprocessing fully handled

--- utils/tokenizer.py
+++ utils/tokenizer.py
@@ -4,8 +4,6 @@ import itertools
 import nltk
 import numpy as np
 
-import pprint as pp
-
 
 def tag_sentences(stories):
     """
@@ -26,48 +24,6 @@ def tag_sentences(stories):
     return tagged_stories
 
 
-def word_tokenizer(tagged_stories):
-    """
-    Take a list of stories and tokenize each sentence of each story
-    """
-    return [[nltk.word_tokenize(sent) for sent in story]
-            for story in tagged_stories]
-
-
-def build_indexes(token_sents, vocab_size=8000):
-    """
-    Take a list of tokenized stories to build vocab indexes
-    """
-    # Token for infrequently used words
-    unknown_token = 'UNKNOWN_TOKEN'
-
-    # Count word frequencies
-    word_freq = nltk.FreqDist(itertools.chain(*token_sents))
-    n_words = len(word_freq.items())
-    print('There are {} unique word tokens\n'.format(n_words))
-
-    # Pull most common words based on defined vocab size
-    if n_words > vocab_size:
-        vocab = word_freq.most_common(vocab_size - 1)
-        print('Vocabulary size is {}\n'.format(vocab_size))
-    else:
-        vocab = word_freq.most_common(n_words - 1)
-        print('Vocabulary size is {}\n'.format(n_words))
-
-    # Build indexes
-    idx2word = [word[0] for word in vocab]
-    idx2word.append(unknown_token)
-    word2idx = dict([(w, i) for i, w in enumerate(idx2word)])
-    print('The least frequent word is {}, appearing {} time(s)\n'.format(
-        vocab[-1][0], vocab[-1][1])
-    )
-
-    # Replace words left out of vocabulary, if any, with "unknown token"
-    for i, sent in enumerate(token_sents):
-        token_sents[i] = [w if w in word2idx else unknown_token for w in sent]
-    return (word2idx, idx2word)
-
-
 def load_stories(csv_path):
     print('Reading ROC Stories...\n')
     with open(csv_path, 'r') as csvfile:
@@ -78,23 +34,70 @@ def load_stories(csv_path):
     return tagged_stories
 
 
-def create_training_data(indexes, stories):
-    word2idx, idx2word = indexes[0], indexes[1]
-    X_train = np.asarray([[word2idx[w] for w in sent[:-1]] for sent in tokens])
-    y_train = np.asarray([[word2idx[w] for w in sent[1:]] for sent in tokens])
-    return (X_train, y_train)
+def word_tokenizer(tagged_stories):
+    """
+    Take a list of stories and tokenize each sentence of each story
+    """
+    return [[nltk.word_tokenize(sent) for sent in story]
+            for story in tagged_stories]
 
 
-def main(story_path):
-    stories = load_stories(story_path)
-    tokenized_stories = word_tokenizer(stories)
-    tokenized_sents = list(itertools.chain.from_iterable(tokenized_stories))
-    (word2idx, idx2word) = build_indexes(tokenized_sents)
-    (X_train, y_train) = create_training_data()
+def create_training_data(word2idx, tokenized_sents):
     X_train = np.asarray(
         [[word2idx[w] for w in sent[:-1]] for sent in tokenized_sents])
     y_train = np.asarray(
         [[word2idx[w] for w in sent[1:]] for sent in tokenized_sents])
+    return (X_train, y_train)
+
+
+def preprocess(story_path, vocab_size=8000):
+    """
+    Load up ROC Stories and return two-way indexes
+    """
+    tagged_stories = load_stories(story_path)
+    tokenized_stories = word_tokenizer(tagged_stories)
+    tokenized_sents = list(itertools.chain.from_iterable(tokenized_stories))
+
+    # Count word frequencies
+    word_freq = nltk.FreqDist(itertools.chain(*tokenized_sents))
+    n_words = len(word_freq.items())
+    print('There are {} unique word tokens\n'.format(n_words))
+
+    print('Building word-index dictionaries...')
+    # Pull most common words based on defined vocab size
+    if n_words > vocab_size:
+        vocab = word_freq.most_common(vocab_size - 1)
+        print('Vocabulary size is {}'.format(vocab_size))
+    else:
+        vocab = word_freq.most_common(n_words - 1)
+        print('Vocabulary size is {}'.format(n_words))
+
+    # Special token for infrequently used words
+    unknown_token = 'UNKNOWN_TOKEN'
+
+    # Build dictionaries: word <-> idx
+    idx2word = [word[0] for word in vocab]
+    idx2word.append(unknown_token)
+    word2idx = dict([(w, i) for i, w in enumerate(idx2word)])
+    print('The least frequent word is {}, appearing {} time(s)\n'.format(
+        vocab[-1][0], vocab[-1][1])
+    )
+
+    # Replace words left out of vocabulary, if any, with "unknown token"
+    for i, sent in enumerate(tokenized_sents):
+        tokenized_sents[i] = [w if w in word2idx else unknown_token for w in sent]
+
+    # Vectorize tokenized sentences
+    print('Vectorizing...\n')
+    (X_train, y_train) = create_training_data(word2idx, tokenized_sents)
+
+    # Return two tuples: indexes and training data
+    return (word2idx, idx2word), (X_train, y_train)
+
+
+def main(story_path):
+    indexes, train_set = preprocess(story_path)
+    X_train, y_train = train_set[0], train_set[1]
     print('Train Dim: {}  |  Sample: {}'.format(X_train.shape, X_train[0]))
     print('Label Dim: {}  |  Sample: {}'.format(y_train.shape, y_train[0]))
 
