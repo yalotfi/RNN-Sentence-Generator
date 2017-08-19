9b2f7ebf8b6ae14df5c2d284acc2435500fbbd96
Yaseen Lotfi <yalotfi@outlook.com>
2017-08-18 22:46:55 -0400

    shape sequences and test two LSTM architectures

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -5,11 +5,12 @@ import nltk
 import numpy as np
 
 # TESTING DEPENDENCIES --------------------------
-import random
-import sys
-import pprint as pp
+# import random
+# import sys
+# import pprint as pp
 
 from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
 
 from keras.models import Sequential
 from keras.layers.embeddings import Embedding
@@ -22,46 +23,39 @@ from keras.optimizers import RMSprop
 
 # --------------------------------------------
 # TESTING FUNCTIONS --------------------------
-def build_rnn(vocab_size, embeddings, hh, batch_input_shape):
-    '''
-    --- Compile a Keras RNN model ---
-    Very simple LSTM with a single layer of hidden units that are fully
-    connected to a softmax layer (crossentropy loss). It takes an input tensor
-    of dims: (max sequence length, vocabulary size). Loss is minimized by
-    RMSprop which is part of a family of stochastic gradient descent (SGD)
-    algorithims that use adaptive learning rates that decay over time.
-    '''
+# --------------------------------------------
+def lstm_basic(hidden, max_len, vocab_size):
+    model = Sequential()
+    model.add(LSTM(hidden, input_shape=(max_len, vocab_size)))
+    model.add(Dense(vocab_size, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer=RMSprop(lr=0.01))
+    return  model
+
+
+def lstm_embedding(batch_size, timesteps, vocab_size, embeddings, hidden):
     print("Building Model...")
     model = Sequential()
-    model.add(Embedding(batch_input_shape=batch_input_shape,
+    model.add(Embedding(batch_input_shape=(batch_size, timesteps),
                         input_dim=vocab_size + 1,
                         output_dim=embeddings,
                         mask_zero=True))
-    model.add(LSTM(hh,
-                   activation='relu',
-                   return_sequences=True,
-                   stateful=True))
-    model.add(Dense(vocab_size + 1))
-    model.add(Activation('softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer=RMSprop(lr=0.01))
+    model.add(LSTM(hidden, return_sequences=True, stateful=True))
+    model.add(Dense(vocab_size + 1, activation='softmax'))
+    model.compile(loss='sparse_categorical_crossentropy',
+                  optimizer=RMSprop(lr=0.01))
     return model
 
 
-def sample(preds, temperature=1.0):
-    '''
-    --- Helper function to sample from softmax output ---
-    Takes input prediction vector and a temperature parameter. Larger temp
-    will generate more diverse samples at the cost of accuracy while small
-    values will be more confident but conservative.
-    '''
-    preds = np.asarray(preds).astype('float64')
-    preds = np.log(preds) / temperature
-    exp_preds = np.exp(preds)
-    preds = exp_preds / np.sum(exp_preds)
-    probas = np.random.multinomial(1, preds, 1)
-    return np.argmax(probas)
+# def sample(preds, temperature=1.0):
+#     preds = np.asarray(preds).astype('float64')
+#     preds = np.log(preds) / temperature
+#     exp_preds = np.exp(preds)
+#     preds = exp_preds / np.sum(exp_preds)
+#     probas = np.random.multinomial(1, preds, 1)
+#     return np.argmax(probas)
 # --------------------------------------------
 # TESTING FUNCTIONS --------------------------
+# --------------------------------------------
 
 
 def tag_sentences(stories):
@@ -84,7 +78,6 @@ def tag_sentences(stories):
 
 
 def load_stories(csv_path):
-    print('Reading ROC Stories...\n')
     with open(csv_path, 'r') as csvfile:
         story_reader = csv.reader(csvfile, delimiter=',')
         story_lists = [row for row in story_reader]
@@ -101,22 +94,7 @@ def word_tokenizer(tagged_stories):
             for story in tagged_stories]
 
 
-def create_training_data(word2idx, tokenized_sents):
-    X_train = np.asarray(
-        [[word2idx[w] for w in sent[:-1]] for sent in tokenized_sents])
-    y_train = np.asarray(
-        [[word2idx[w] for w in sent[1:]] for sent in tokenized_sents])
-    return (X_train, y_train)
-
-
-def preprocess(story_path, vocab_size=8000):
-    """
-    Load up ROC Stories and return two-way indexes
-    """
-    tagged_stories = load_stories(story_path)
-    tokenized_stories = word_tokenizer(tagged_stories)
-    tokenized_sents = list(itertools.chain.from_iterable(tokenized_stories))
-
+def build_indexes(tokenized_sents, vocab_size):
     # Count word frequencies
     word_freq = nltk.FreqDist(itertools.chain(*tokenized_sents))
     n_words = len(word_freq.items())
@@ -126,60 +104,86 @@ def preprocess(story_path, vocab_size=8000):
     # Pull most common words based on defined vocab size
     if n_words > vocab_size:
         vocab = word_freq.most_common(vocab_size - 1)
-        print('Vocabulary size is {}'.format(vocab_size))
+        print('\tVocabulary size is {}'.format(vocab_size))
     else:
         vocab = word_freq.most_common(n_words - 1)
-        print('Vocabulary size is {}'.format(n_words))
+        print('\tVocabulary size is {}'.format(n_words))
 
-    # Special token for infrequently used words
+    # Special token for infrequently used words and sequence pads
     unknown_token = 'UNKNOWN_TOKEN'
+    padding_token = 'PADDING'
 
     # Build dictionaries: word <-> idx
     idx2word = [word[0] for word in vocab]
     idx2word.append(unknown_token)
+    idx2word.append(padding_token)
     word2idx = dict([(w, i) for i, w in enumerate(idx2word)])
-    print('The least frequent word is {}, appearing {} time(s)\n'.format(
+    print('\tThe least frequent word is {}, appearing {} time(s)\n'.format(
         vocab[-1][0], vocab[-1][1])
     )
 
     # Replace words left out of vocabulary, if any, with "unknown token"
     for i, sent in enumerate(tokenized_sents):
-        tokenized_sents[i] = [w if w in word2idx else unknown_token for w in sent]
+        tokenized_sents[i] = [
+            w if w in word2idx else unknown_token for w in sent]
 
-    # Vectorize tokenized sentences
-    print('Vectorizing...\n')
+    # Return the two dicts
+    return word2idx, idx2word
+
+
+def create_training_data(word2idx, tokenized_sents):
+    # Vectorize tokens and pad to max sequence length
+    max_seq = max([len(sent) for sent in tokenized_sents])
+    indexed = [[word2idx[w] for w in sent] for sent in tokenized_sents]
+    pad_vecs = pad_sequences(indexed, max_seq, value=len(word2idx))
+
+    # Create feature and label sets
+    X_train = np.asarray([[w for w in sent[:-1]] for sent in pad_vecs])
+    y_train = np.asarray([[w for w in sent[1:]] for sent in pad_vecs])
+
+    # Reshape to 3D tensor
+    tensor_dim = (len(pad_vecs), max_seq - 1, 1)
+    return (X_train.reshape(tensor_dim), y_train.reshape(tensor_dim))
+
+
+def preprocess(story_path, vocab_size=8000):
+    """
+    Process ROC Stories from tokenization through vectorization
+    """
+    # Load raw text, tokenize sentences and words, then index
+    print('Parsing stories...\n')
+    tagged_stories = load_stories(story_path)
+    tokenized_stories = word_tokenizer(tagged_stories)
+    tokenized_sents = list(itertools.chain.from_iterable(tokenized_stories))
+    (word2idx, idx2word) = build_indexes(tokenized_sents, vocab_size)
+
+    # Vectorize tokenized sentences into training data
+    print('Vectorizing Text Sequences...')
     (X_train, y_train) = create_training_data(word2idx, tokenized_sents)
+    print('\tFeature Size: {}  |  Label size {}\n'.format(
+        X_train.shape, y_train.shape))
 
-    # Return two tuples: indexes and training data
+    # Return two tuples: vocab dictionaries and training data
     return (word2idx, idx2word), (X_train, y_train)
 
 
 def main(story_path):
-    indexes, train_set = preprocess(story_path)
-    X_train, y_train = train_set[0], train_set[1]
-    word2idx, idx2word = indexes[0], indexes[1]
+    (word2idx, idx2word), (X_train, y_train) = preprocess(story_path)
 
     # --------------------------------------------------
     # TESTING ------------------------------------------
     # --------------------------------------------------
-    # Model hyperparameters
-    max_len = max([len(sequence) for sequence in X_train])
-    vocab_size = len(word2idx)
-    embeddings = 300
-    hidden = 128
-    batch_size = 32
+    # Hyperparameters
+    batch_size = 64
+    embeddings = 64
+    max_len = X_train.shape[1]
     steps = max_len - 1
-
-    # Create (m, n) padded input matrices...
-    X_train = pad_sequences(X_train, maxlen=max_len)[:, :-1]
-    y_train = pad_sequences(y_train, maxlen=max_len)[:, 1:]
-    print(X_train[0: 0 + batch_size].shape)
+    vocab_size = len(word2idx)
+    hidden = 128
 
     # Compile and summarize architecture
-    model = build_rnn(vocab_size=vocab_size,
-                      embeddings=embeddings,
-                      hh=hidden,
-                      batch_input_shape=(batch_size, steps))
+    model = lstm_embedding(batch_size, steps, vocab_size, embeddings, hidden)
+    # model = lstm_basic(hidden, max_len, vocab_size)
     print(model.summary())
 
     # Training
@@ -188,9 +192,9 @@ def main(story_path):
               batch_size=batch_size,
               epochs=epochs,
               verbose=1)
-    model.save_weights('rnn_weights.h5')
+    # model.save_weights('rnn_weights.h5')
 
-    # # Generator hyperparameters
+    # # Generate
     # max_iter = 30
     # sampling_freedom = [0.5, 1.0, 1.2]
     # for iteration in range(1, max_iter + 1):
