a61d17c7df0c0529d3e2af37555ba09b44163b52
Yaseen Lotfi <yalotfi@outlook.com>
2017-09-09 22:31:59 -0400

    train word2vec skip gram model

--- embeddings/Word2Vec/similarity.py
+++ embeddings/Word2Vec/similarity.py
@@ -1,24 +1,26 @@
 import os
 import pprint as pp
+import time as t
+
 from gensim.models import Word2Vec
 
 
 def main():
-    model = Word2Vec.load(os.path.join('models', 'vectors'))
-    test_words = ['food', 'river', 'bank', 'swim']
-    for word in test_words:
-        print(word)
-        pp.pprint(model.similar_by_word(word, topn=10))
-    print(model.similarity('bank', 'money'))
-
-    '''
-    # Check scores between bank and:
-    check
-    job
-    loan
-    funds
-    account
-    '''
+    print('Looking at these word vectors:')
+    test_words = ['money', 'job', 'account', 'fund', 'bank']
+    print(test_words)
+    windows = [3, 5, 7, 9, 11]
+    tic = t.time()
+    for w in windows:
+        print('\nLOADING MODEL WITH WINDOW SIZE {}\n'.format(w))
+        fname = 'vecs_300_window' + str(w)
+        model = Word2Vec.load(os.path.join('models', 'embeddings', fname))
+        for word in test_words:
+            print(word)
+            pp.pprint(model.similar_by_word(word, topn=10))
+    toc = t.time() - tic
+    print("Runtime: {}".format(toc))
+    # print(model.similarity('bank', 'money'))
 
 
 if __name__ == '__main__':
--- embeddings/Word2Vec/train_word2vec.py
+++ embeddings/Word2Vec/train_word2vec.py
@@ -2,7 +2,7 @@ import os
 import gensim
 import logging
 
-from tempfile import mkstemp
+# from tempfile import mkstemp
 from nltk import sent_tokenize
 from nltk import word_tokenize
 
@@ -13,19 +13,22 @@ def main():
         format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO
     )
     file_path = os.path.join('data', 'roc_text_full.txt')
-    model_path = os.path.join('models', 'vectors')
-    fs, temp_path = mkstemp('word2vec_temp')
-
     # preprocessing
     corpus = open(file_path).read().lower()
     sentences = [word_tokenize(sent) for sent in sent_tokenize(corpus)]
-    # look into lemmatization
-    # look into stop words, frequently used words like 'the'
+    windows = [3, 5, 7, 9, 11]
+    for w in windows:
+        fname = 'vecs_300_window' + str(w)
+        model_path = os.path.join('models', 'embeddings', fname)
+        # fs, temp_path = mkstemp('word2vec_temp')
 
-    # train and save temp file
-    model = gensim.models.Word2Vec(
-        sentences, size=64, window=5, min_count=5, workers=4)
-    model.save(model_path)
+        # look into lemmatization
+        # look into stop words, frequently used words like 'the'
+
+        # train and save temp file
+        model = gensim.models.Word2Vec(
+            sentences, size=300, window=w, min_count=5, workers=4)
+        model.save(model_path)
 
 
 if __name__ == '__main__':
