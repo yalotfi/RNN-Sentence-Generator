17ec016d8733575e951a5a67decd50712afe9dc9
Yaseen Lotfi <yalotfi@outlook.com>
2017-10-01 19:43:54 -0400

    train embeddings on combined roc and grimm stories

--- embeddings/Word2Vec/train_word2vec.py
+++ embeddings/Word2Vec/train_word2vec.py
@@ -2,7 +2,6 @@ import os
 import gensim
 import logging
 
-# from tempfile import mkstemp
 from nltk import sent_tokenize
 from nltk import word_tokenize
 
@@ -12,20 +11,14 @@ def main():
     logging.basicConfig(
         format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO
     )
-    file_path = os.path.join('data', 'roc_text_full.txt')
+    file_path = os.path.join('data', 'roc_grimm_corpus.txt')
     # preprocessing
     corpus = open(file_path).read().lower()
     sentences = [word_tokenize(sent) for sent in sent_tokenize(corpus)]
     windows = [3, 5, 7, 9, 11]
     for w in windows:
         fname = 'vecs_300_window' + str(w)
-        model_path = os.path.join('models', 'embeddings', fname)
-        # fs, temp_path = mkstemp('word2vec_temp')
-
-        # look into lemmatization
-        # look into stop words, frequently used words like 'the'
-
-        # train and save temp file
+        model_path = os.path.join('models', 'embeddings', 'roc_grimm', fname)
         model = gensim.models.Word2Vec(
             sentences, size=300, window=w, min_count=5, workers=4)
         model.save(model_path)
